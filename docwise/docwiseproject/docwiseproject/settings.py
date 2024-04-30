edd15c9814b8ac9b82a38a5f8f4d8ce34baa0946
g4mrpr0n <georgeana.globa@gmail.com>
2024-04-30 18:11:30 +0300

    dotenv

--- docwise/docwiseproject/docwiseproject/settings.py
+++ docwise/docwiseproject/docwiseproject/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9x$c#k^jg(5(noao&*jgbvj_wqt9p72=reco&-zs$-#4d8ss9b'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = []
 
